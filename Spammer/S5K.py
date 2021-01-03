de9aeca65e890dd4500ef41002f3a2949abacd5f
LionDoesThings <48542813+LionDoesThings@users.noreply.github.com>
2021-01-03 13:44:37 +0800

    Update S5K.py

--- Spammer/S5K.py
+++ Spammer/S5K.py
@@ -1,58 +1,92 @@
-import keyboard
-import yaml
 from pynput.keyboard import Key, Controller
+import keyboard
+import requests
+import yaml
+import os
 
-print(f'''S5K Made By LionDoesThings
-YouTube: https://www.youtube.com/channel/UCzpt7vwCAola2IJ1c4rD1aQ
-''')
+# Always Changing Variables #
+ver = 0
+#                           #
 
-# yaml
-def readYamlFile(arg):
-    with open('settings.yaml') as f:
-        data = yaml.load(f.read(), Loader=yaml.FullLoader)
-    if arg == "start":
-        return data["Hotkeys"]["Start"]
-    elif arg == "stop":
-        return data["Hotkeys"]["Stop"]
-    elif arg == "cooldown":
-        return data["Misc"]["SpamCooldown"]
+print('S5K Made By LionDoesThings\nYouTube: https://www.youtube.com/channel/UCzpt7vwCAola2IJ1c4rD1aQ\n')
 
 
-# Variables
-start = readYamlFile("start")
-stop = readYamlFile("stop")
-spamcd = readYamlFile("cooldown")
-spamming = False
-started = True
-ctrl = Controller
+def new_version_checker():
+    version_url = 'https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/version'
+    return requests.get(version_url).text
 
-# Questions
-what_to_spam = input(f'What Do You Want To Spam: ') + f'\n'
-times_to_spam = int(input(f'How Many Times Do You Want To Spam: '))
-print('')
-print(f'Press "{start}" To Start Spamming')
-print(f'Press "{stop}" To Emergency Stop')
 
-# Main Code
-while not keyboard.is_pressed(stop):
+def new_settings_version_checker():
+    settings_version_url = 'https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/settings_version'
+    return requests.get(settings_version_url).text
+
+
+try:
+    with open("settings.yaml", 'r') as f:
+        settings = yaml.load(f, Loader=yaml.FullLoader)
+    start_hk = settings["Hotkeys"]["Start"]
+    stop_hk = settings["Hotkeys"]["Stop"]
+    spam_cd = int(settings["Misc"]["SpamCooldown"])
+    settings_ver = int(settings["Settings_Version"])
+except FileNotFoundError:
+    print('settings.yaml not found!' + '\n' + 'Downloading settings.yaml')
+    os.system("curl -o settings.yaml https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/settings.yaml")
+
+if float(new_version_checker()) > ver:
+    print('New version found but not necessary to update right now' + '\n' + 'Would you like to download the new version now? (y/n)')
+    ans = input('> ').lower()
+    if ans == 'y':
+        print('\nDownloading')
+        os.system("curl -o S5K.py https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/S5K.py")
+    elif ans == 'n':
+        pass
+    else:
+        print('no?')
+        input()
+        exit()
+
+if float(new_settings_version_checker()) > settings_ver:
+    print('New settings version found\nDownloading now')
+    os.system("curl -o settings.yaml https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/settings.yaml")
+
+to_spam = input('What to spam: ') + '\n'
+
+try:
+    times_to_spam = int(input('Times to spam: '))
+    if times_to_spam <= 0:
+        print('not funny stop it')
+        input()
+        exit()
+except ValueError:
+    print('no not funny bruh')
+    input()
+    exit()
+
+print('\n' + 'To start spamming please press "{}"'.format(start_hk) + '\n' + 'To force stop please press "{}"'.format(stop_hk))
+
+ctrl = Controller()
+started = False
+spam_cd_done = True
+spam_cd_ = spam_cd
+while not keyboard.is_pressed(stop_hk):
+    if keyboard.is_pressed(start_hk):
+        started = True
+        ctrl.press(Key.backspace)
+        ctrl.release(Key.backspace)
+
+    if started and times_to_spam > 0 and spam_cd_done:
+        ctrl.type(to_spam)
+        times_to_spam -= 1
+        spam_cd_done = False
 
     if started:
-        cooldown -= 1
+        spam_cd_ -= 1
 
-        if times_to_spam <= 0:
-            started = False
+    if spam_cd_ <= 0:
+        spam_cd_done = True
+        spam_cd_ = spam_cd
 
-        if keyboard.is_pressed(start) and not spamming:
-            spamming = True
-            ctrl.press(Key.backspace)
-            ctrl.release(Key.backspace)
+    if times_to_spam <= 0:
+        break
 
-        if cooldown <= 0 and spamming:
-            ctrl.type(what_to_spam)
-            cooldown = spamcd
-            times_to_spam -= 1
-
-# End
-print(f'')
-print(f'')
-input(f'Press Enter To Close The Program')
+input('\n\n' + 'Spam completed/Force stopped\n' + 'Press Enter To Close The Program')
