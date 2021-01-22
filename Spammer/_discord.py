3bc5aa9d09d46d467855e3a377f4dce3d6ac98fd
LionDoesThings <48542813+LionDoesThings@users.noreply.github.com>
2021-01-22 12:08:29 +0800

    Add files via upload

---
+++ Spammer/_discord.py
@@ -0,0 +1,87 @@
+import requests
+import discord
+import asyncio
+import yaml
+import os
+client = discord.Client()
+channel_id = None
+started = False
+
+
+ver = 0
+
+
+def new_version_checker():
+	version_url = 'https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/discord_version'
+	return requests.get(version_url).text
+
+def new_settings_version_checker():
+	settings_version_url = 'https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/settings_version'
+	return requests.get(settings_version_url).text
+
+
+try:
+	with open("settings.yaml", 'r') as f:
+		settings = yaml.load(f, Loader=yaml.FullLoader)
+	TOKEN = settings["TOKEN"]
+	settings_ver = int(settings["Settings_Version"])
+except FileNotFoundError:
+	print('settings.yaml not found!' + '\n' + 'Downloading settings.yaml')
+	os.system("curl -o settings.yaml https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/settings.yaml")
+
+if float(new_version_checker()) > ver:
+    print('New version found but not necessary to update right now' + '\n' + 'Would you like to download the new version now? (y/n)')
+    ans = input('> ').lower()
+    if ans == 'y':
+        print('\nDownloading')
+        os.system("curl -o S5K.py https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/S5K.py")
+    elif ans == 'n':
+        pass
+    else:
+        print('no?')
+        input()
+        exit()
+
+if float(new_settings_version_checker()) > settings_ver:
+	print('New settings version found\nDownloading now')
+	os.system("curl -o settings.yaml https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/settings.yaml")
+
+to_spam = input('What to spam: ')
+
+try:
+	times_to_spam = int(input('Times to spam: '))
+	if times_to_spam <= 0:
+		print('not funny stop it')
+		input()
+		exit()
+except ValueError:
+	print('no not funny bruh')
+	input()
+	exit()
+
+@client.event
+async def on_ready():
+	print('Ready')
+	print('Say "{0}" In The Place You Want To Spam'.format(to_spam))
+
+@client.event
+async def on_message(message):
+	global started
+	global channel_id
+	global times_to_spam
+	if message.author == client.user:
+		if message.content.startswith(to_spam):
+			if channel_id is None:
+				channel_id = message.channel.id
+			started = True
+	if started and message.channel.id == channel_id and times_to_spam > 0:
+		await message.channel.send(to_spam)
+		times_to_spam -= 1
+	if times_to_spam <= 0:
+		print('Close The Terminal To Exit')
+		input()
+
+
+
+
+client.run(TOKEN, bot=False)
---
+++ Spammer/_general.py
@@ -0,0 +1,90 @@
+from pynput.keyboard import Key, Controller
+import keyboard
+import requests
+import yaml
+import os
+
+
+ver = 0.1
+
+
+def new_version_checker():
+    version_url = 'https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/version'
+    return requests.get(version_url).text
+
+
+def new_settings_version_checker():
+    settings_version_url = 'https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/settings_version'
+    return requests.get(settings_version_url).text
+
+
+try:
+    with open("settings.yaml", 'r') as f:
+        settings = yaml.load(f, Loader=yaml.FullLoader)
+    start_hk = settings["Hotkeys"]["Start"]
+    stop_hk = settings["Hotkeys"]["Stop"]
+    spam_cd = int(settings["Misc"]["SpamCooldown"])
+    settings_ver = int(settings["Settings_Version"])
+except FileNotFoundError:
+    print('settings.yaml not found!' + '\n' + 'Downloading settings.yaml')
+    os.system("curl -o settings.yaml https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/settings.yaml")
+
+if float(new_version_checker()) > ver:
+    print('New version found but not necessary to update right now' + '\n' + 'Would you like to download the new version now? (y/n)')
+    ans = input('> ').lower()
+    if ans == 'y':
+        print('\nDownloading')
+        os.system("curl -o S5K.py https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/S5K.py")
+    elif ans == 'n':
+        pass
+    else:
+        print('no?')
+        input()
+        exit()
+
+if float(new_settings_version_checker()) > settings_ver:
+    print('New settings version found\nDownloading now')
+    os.system("curl -o settings.yaml https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/settings.yaml")
+
+to_spam = input('What to spam: ') + '\n'
+
+try:
+    times_to_spam = int(input('Times to spam: '))
+    if times_to_spam <= 0:
+        print('not funny stop it')
+        input()
+        exit()
+except ValueError:
+    print('no not funny bruh')
+    input()
+    exit()
+
+print('\n' + 'To start spamming please press "{}"'.format(start_hk) + '\n' + 'To force stop please press "{}"'.format(stop_hk))
+
+ctrl = Controller()
+started = False
+spam_cd_done = True
+spam_cd_ = spam_cd
+while not keyboard.is_pressed(stop_hk):
+    if keyboard.is_pressed(start_hk):
+        started = True
+        ctrl.press(Key.backspace)
+        ctrl.release(Key.backspace)
+
+    if started and times_to_spam > 0 and spam_cd_done:
+        ctrl.type(to_spam)
+        times_to_spam -= 1
+        spam_cd_done = False
+
+    if started:
+        spam_cd_ -= 1
+
+    if spam_cd_ <= 0:
+        spam_cd_done = True
+        spam_cd_ = spam_cd
+
+    if times_to_spam <= 0:
+        input('\n\n' + 'Spam completed\n' + 'Press Enter To Close The Program')
+        exit()
+
+input('\n\n' + 'Force stopped\n' + 'Press Enter To Close The Program')
