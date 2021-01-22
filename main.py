b43c916b9748b38935d0631dbcddf8311a5b374f
LionDoesThings <48542813+LionDoesThings@users.noreply.github.com>
2021-01-22 12:33:37 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+os.system('pip install pynput requests keyboard discord asyncio')
+os.system('cls')
+print('S5K Made By LionDoesThings\nYouTube: https://www.youtube.com/channel/UCzpt7vwCAola2IJ1c4rD1aQ\n')
+print('''Options:
+[0] General Spammer
+[1] Direct Discord Spammer
+''')
+a = int(input('> '))
+if a == 0:
+	try:
+		os.system('py _general.py')
+	except FileNotFoundError:
+		os.system('curl -o _general.py https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/_general.py')
+		os.system('py _general.py')
+elif a == 1:
+	try:
+		os.system('py _discord.py')
+	except FileNotFoundError:
+		os.system('curl -o _discord.py https://raw.githubusercontent.com/LionDoesThings/S5K-v3-aka-Yasuo/main/Spammer/_discord.py')
+		os.system('py _discord.py')
+else:
+	input('no\n')
+	exit()
